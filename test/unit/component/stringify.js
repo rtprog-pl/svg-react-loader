f648d86b1814cdc0ff88af4ac854cf71e133746e
Ryszard Trojnacki <ryszard.trojnacki@gmail.com>
2025-08-14 14:28:09 +0200

    Changed code to use function default parameters instead of deprecated `defaultProps`.

--- test/unit/component/stringify.js
+++ test/unit/component/stringify.js
@@ -1,5 +1,6 @@
 /*globals describe, it*/
 require('should');
+const read = require("../../../lib/util/read-file");
 
 describe('svg-react-loader/lib/component/stringify', () => {
     const toModule  = require('../../../lib/component/stringify')({
@@ -38,14 +39,13 @@ describe('svg-react-loader/lib/component/stringify', () => {
                         should.
                         equal(
                             'var React = require(\'react\');\n\n' +
-                            'function SvgReactComponent (props) {\n' +
-                            '    return React.createElement("svg",props,' +
+                            'function SvgReactComponent ({"version":"1.1","x":"0px","y":"0px","viewBox":"0 0 16 16","enableBackground":"new 0 0 16 16","xmlSpace":"preserve","className":"simple", ...rest }) {\n' +
+                            '    return React.createElement("svg", {version, x, y, viewBox, enableBackground, xmlSpace, className, ...rest },' +
                             '[React.createElement("rect",{"x":"0","y":"0",' +
                             '"width":"16","height":"16","fill":"#fff","key":0}),' +
                             'React.createElement("text",{"key":1},"Foobar")' +
                             ']);\n' +
                             '}\n\n' +
-                            'SvgReactComponent.defaultProps = ' + JSON.stringify(expectedProps) + ';\n\n' +
                             'module.exports = SvgReactComponent;\n\n' +
                             'SvgReactComponent.default = SvgReactComponent;\n'
                         );
@@ -53,5 +53,29 @@ describe('svg-react-loader/lib/component/stringify', () => {
                 (error) => { throw error; },
                 done
             );
+    });
+
+    it('should return the correct string for a component function for SVG without default props', (done) => {
+        read('test/samples/text.svg').
+        flatMap(xmlParser).
+        map(sanitize).
+        map(toModule).
+        subscribe(
+            (result) => {
+                result.
+                should.
+                equal(
+                    'var React = require(\'react\');\n\n' +
+                    'function SvgReactComponent (props) {\n' +
+                    '    return React.createElement("svg", props,' +
+                    'React.createElement("g",null,[React.createElement("title",{"key":0},"The Title"),React.createElement("text",{"x":"20","y":"20","key":1},"Text")]));\n' +
+                    '}\n\n' +
+                    'module.exports = SvgReactComponent;\n\n' +
+                    'SvgReactComponent.default = SvgReactComponent;\n'
+                );
+            },
+            (error) => { throw error; },
+            done
+        );
     });
 });
